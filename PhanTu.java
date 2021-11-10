60ca1fba1ae84d5767f71f342fc2bba334857836
rivens-coding <78080819+rivens-coding@users.noreply.github.com>
2021-11-10 23:46:44 +0700

    Update PhanTu.java

--- PhanTu.java
+++ PhanTu.java
@@ -1,27 +1,56 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
-public class PhanTu<T>{
+import edu.princeton.cs.algs4.*;
+
+public class PhanTu<T extends Comparable<T>>{
     private T t;
-    private T[] arr;
+    public PhanTu(){}
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+
     public boolean isGreaterThan(T a) {
-        int intt = (int)t;
-        int inta = (int)a;
-        if(Integer.compare(intt,inta)==1)
+        if(t.compareTo(a)>0)
             return true;
         return false;
     }
+
+    public static void fileIO(String input,String output){
+            In in = new In(input);
+            Out out = new Out(output);
+            List<Integer> numbers = new ArrayList<>();
+            while(!in.isEmpty()) {
+                int a = in.readInt();
+                numbers.add(a);
+            }
+            Collections.sort(numbers);
+            for(Integer integer : numbers){
+                out.println(integer.toString());
+            }
+
+    }
+
     public static void main(String[] args){
-        File file = new File(”data.txt”);
-        FileReader reader = new FileReader(file);
-        BufferedReader in = new BufferedReader(reader);
-        int a;
-        try {
-            a = in.read();
-        }
-        catch (IOException e)
+        Scanner sc = new Scanner(System.in);
+
+        Integer number = sc.nextInt();
+        PhanTu<Integer> n = new PhanTu<>();
+        n.setT(number);
+        Integer m = sc.nextInt();
+        System.out.println(n.isGreaterThan(m));
+
+        String input = args[0];
+        String output = args[1];
+        fileIO(input,output);
     }
 }
 
